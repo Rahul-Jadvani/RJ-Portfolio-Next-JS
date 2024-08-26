cbc375633ac76ae4e5001cf75250fabcab4ef757
Rahul Jadvani <rahuljadvani24@gmail.com>
2024-08-26 20:20:47 +0530

    added psuedo resume

--- src/components/ui/buttonui.tsx
+++ src/components/ui/buttonui.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Buttonui = ({ title }: ButtonProps) => {
     return (
-        <button className="inline-flex h-6 animate-shimmer items-center justify-center rounded-lg border border-accent bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-4 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-accent-hover focus:ring-offset-2 focus:ring-offset-accent-hover">
+        <button className="inline-flex h-6 animate-shimmer items-center justify-center rounded-lg border border-accent bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-4 font-medium text-white transition-colors focus:outline-none  focus:ring-accent-hover focus:ring-offset-2 focus:ring-offset-accent-hover hover:scale-110">
             {title}
         </button>
     );
